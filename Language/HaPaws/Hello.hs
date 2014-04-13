edd48272daee61f1607cb505998dab5636d0628f
Devyn Cairns <devyn.cairns@gmail.com>
2014-04-13 05:17:20 -0700

    Oh, right, we have to document the useless module too!

--- Language/HaPaws/Hello.hs
+++ Language/HaPaws/Hello.hs
@@ -1,7 +1,10 @@
+-- | A useless module while I test out all of the boilerplate (specs,
+-- documentation, etc.) and get that ready.
 module Language.HaPaws.Hello (
   hello
 ) where
 
+-- | Returns a string greeting the argument, probably someone or something's
+-- name, with 'Hello'.
 hello :: String -> String
-
 hello = ("Hello, " ++) . (++ "!")
